c339a088c8847fc55b5bf4f5e29be811abe447a2
maragos <Emmanouil.Maragkos@nokia.com>
2022-11-24 11:38:02 +0200

    Add grpc_callback_exmple

---
+++ grpc_callback_example/README.md
@@ -0,0 +1,3 @@
+protoc --cpp_out=. hello_world.proto
+protoc --grpc_out=. --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin hello_world.proto
+g++ -o hello_world hello_world.cpp hello_world.pb.cc hello_world.grpc.pb.cc -lgrpc++
---
+++ grpc_callback_example/hello_world.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include "hello_world.grpc.pb.h"
+
+using grpc::CallbackServerContext;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::Status;
+using HW::HelloNote;
+using std::chrono::system_clock;
+
+
+class HelloWorldImpl final : public HelloWorld::CallbackService {
+ public:
+  explicit HelloWorldImpl() {
+  }
+
+
+  grpc::ServerBidiReactor<HelloNote, HelloNote>* RouteChat(CallbackServerContext* context) override {
+
+    class Chatter : public grpc::ServerBidiReactor<HelloNote, HelloNote> {
+     public:
+      Chatter(absl::Mutex* mu, std::vector<HelloNote>* received_notes)
+          : mu_(mu), received_notes_(received_notes) {
+        StartRead(&note_);
+      }
+      void OnDone() override { delete this; }
+      void OnReadDone(bool ok) override {
+        if (ok) {
+          // Unlike the other example in this directory that's not using
+          // the reactor pattern, we can't grab a local lock to secure the
+          // access to the notes vector, because the reactor will most likely
+          // make us jump threads, so we'll have to use a different locking
+          // strategy. We'll grab the lock locally to build a copy of the
+          // list of nodes we're going to send, then we'll grab the lock
+          // again to append the received note to the existing vector.
+          mu_->Lock();
+          std::cout << "OnReadDone" << std::endl;
+          mu_->Unlock();
+          notes_iterator_ = to_send_notes_.begin();
+          NextWrite();
+        } else {
+          Finish(Status::OK);
+        }
+      }
+      void OnWriteDone(bool /*ok*/) override { NextWrite(); }
+
+     private:
+      void NextWrite() {
+        if (notes_iterator_ != to_send_notes_.end()) {
+          StartWrite(&*notes_iterator_);
+          notes_iterator_++;
+        } else {
+          mu_->Lock();
+          received_notes_->push_back(note_);
+          mu_->Unlock();
+          StartRead(&note_);
+        }
+      }
+      HelloNote note_;
+      absl::Mutex* mu_;
+      std::vector<HelloNote>* received_notes_;
+      std::vector<HelloNote> to_send_notes_;
+      std::vector<HelloNote>::iterator notes_iterator_;
+    };
+    return new Chatter(&mu_, &received_notes_);
+  }
+
+ private:
+  std::vector<Feature> feature_list_;
+  absl::Mutex mu_;
+  std::vector<HelloNote> received_notes_ ABSL_GUARDED_BY(mu_);
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  HelloWorldImpl service();
+
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  // Expect only arg: --db_path=path/to/route_guide_db.json.
+  RunServer();
+
+  return 0;
+}
